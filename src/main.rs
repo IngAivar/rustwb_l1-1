259af708a64d986005a06158ec606212a12dc735
IngAivar <MiniGan067@yandex.ru>
2024-08-29 10:30:42 +0300

    Создание L1.1

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+// Трейт Action с методом say оформляющий приветствие
+pub trait Action {
+    fn person_name(&self) -> String;
+
+    fn say(&self) -> String {
+        format!("Hello, {}", self.person_name())
+    }
+}
+
+// Структура Person с полем person_name в которой будет храниться выводимое имя
+pub struct Person {
+    person_name: String,
+}
+
+// Трайт Action для структуры Person передающий имя методу say
+impl Action for Person {
+    fn person_name(&self) -> String {
+        format!("{}", self.person_name)
+    }
+}
+
+fn main() {
+    // Экземпляр структуры Person
+    let person_inst = Person {
+        person_name: String::from("Vlad")
+    };
+
+    // Сохранение приветствия
+    let greeting = person_inst.say();
+
+    println!("{}", greeting);
+}
